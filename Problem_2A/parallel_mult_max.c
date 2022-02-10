a9b1193c43e50080868b8037c72b9a02c5ae980f
Parker Brandt <parker.brandt101@gmail.com>
2022-02-09 22:33:15 -0600

    Created basic outline of program

---
+++ Problem_2A/parallel_mult_max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <omp.h>
+
+#define DEBUG 0
+#define BUF_SIZE 1024
+
+int main(int argc, char** argv) {
+
+    // Catch console errors
+    if(argc != 10) {
+
+    }
+
+    
+}
