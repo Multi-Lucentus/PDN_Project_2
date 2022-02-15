b8be06a6da6f7d43142ae4c1156ad05290f11d5c
Parker Brandt <parker.brandt101@gmail.com>
2022-02-15 00:47:15 -0600

    Renamed the encrypt file and created a basic outline to test

--- Problem_3/encrypt_parallel.c
+++ Problem_3/encrypt_parallel.c
@@ -15,10 +15,10 @@
 
 int main (int argc, char *argv[])
 {
-    // Catch console errors
+    //  Catch console errors
     //  Make sure you include the # of threads and your output time file.
-    if (argc != 4) {
-        printf("USE LIKE THIS: encrypt_serial key input_text.txt output_text.txt\n");
+    if (argc != 5) {
+        printf("USE LIKE THIS: encrypt_serial key input_text.txt output_text.txt num_threads\n");
         return EXIT_FAILURE;
     }
 
@@ -32,6 +32,8 @@ int main (int argc, char *argv[])
     // Open the output, encrypted text file
     FILE* outputFile = fopen(argv[3], "w");
 
+    // Get the number of threads
+    int thread_count = strtol(arg[4], NULL, 10);
 
     // Allocate and open a buffer to read in the input
     fseek(inputFile, 0L, SEEK_END);
@@ -69,10 +71,13 @@ int main (int argc, char *argv[])
 
     // ----> Begin Encryption <----- //
     // Encrypt the buffer into the encrypted_buffer
+    // TODO: Distribute plaintext among threads
+#   pragma omp parallel for num_threads(thread_count)
     for (int i = 0; i<lSize; i++) {
         // encrypted_buffer[i] = ??? ;  // TODO: Encrypt a character from the input buffer.
+        encrypted_buffer[i] = buffer[i] + key;
     }
-    if (DEBUG) printf("Values encypted! \n");
+    if (DEBUG) printf("Values encrypted! \n");
 
     // Print to the output file
     for (int i = 0; i<lSize; i++) {
